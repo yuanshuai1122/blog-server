861cde658ef46c8e40488fe8a46d6c43ce97dd6f
yuanshuai <yuanshuai@gyyx.cn>
2022-12-01 12:30:25 +0800

    .env

---
+++ initializers/env.go
@@ -0,0 +1,8 @@
+package initializers
+
+import "github.com/joho/godotenv"
+
+// LoadEnv 加载环境变量 默认 .env
+func LoadEnv() {
+	godotenv.Load()
+}
--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"blog-server/initializers"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+// 初始化环境变量
+func init() {
+	initializers.LoadEnv()
+}
 
 func main() {
 	fmt.Println("Hello Blog!")
+
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello World!",
+		})
+	})
+
+	err := r.Run()
+	if err != nil {
+		return
+	}
 }
